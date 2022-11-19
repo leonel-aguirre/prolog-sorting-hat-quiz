fc0c981cf090225dfecc5e6d0ec1ba1906153664
Leonel Aguirre <leonel.aguirre77@gmail.com>
2022-11-19 00:58:09 -0600

    :sparkles: Feature test modules added.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,27 @@
 import "./App.scss"
 
-function App() {
+import { SessionProvider } from "../Context/SessionProvider"
+import Question from "./PrologTests/Question"
+import Option from "./PrologTests/Option"
+
+const App = () => {
   return (
-    <div className="app">
-      <h1 className="app__text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et veritatis
-        eos id minima est. Nisi vitae dignissimos voluptatum atque autem?
-        Distinctio eligendi quasi possimus, totam sit dolore laborum at. Atque!
-      </h1>
-    </div>
+    <SessionProvider>
+      <div className="app">
+        <h1 className="app__text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et veritatis
+          eos id minima est. Nisi vitae dignissimos voluptatum atque autem?
+          Distinctio eligendi quasi possimus, totam sit dolore laborum at.
+          Atque!
+        </h1>
+
+        <div className="app__prolog-test-container">
+          <Question questionID={"q1"} />
+          <Option optionID={"o1"} />
+          <Option optionID={"o2"} />
+        </div>
+      </div>
+    </SessionProvider>
   )
 }
 
