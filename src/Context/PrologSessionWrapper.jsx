c288b1d061a743666e8f6ab4878428fbd496cbba
Leonel Aguirre <leonel.aguirre77@gmail.com>
2022-11-20 08:07:53 -0600

    :zap: PrologSessionWrapper component added.

---
+++ src/Context/PrologSessionWrapper.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+import pl from "tau-prolog"
+import promisesLoader from "tau-prolog/modules/promises"
+import listsLoader from "tau-prolog/modules/lists"
+import randomLoader from "tau-prolog/modules/random"
+
+import KnowledgeBase from "./knowledgeBase.pl"
+import { SET_SESSION } from "../constants"
+import { useSession, useSessionDispatch } from "./SessionProvider"
+
+promisesLoader(pl)
+randomLoader(pl)
+listsLoader(pl)
+
+const prologSession = pl.create()
+
+const PrologSessionWrapper = ({ children }) => {
+  const { session } = useSession()
+  const dispatch = useSessionDispatch()
+
+  const [isProgramLoaded, setIsProgramLoaded] = useState(false)
+
+  // Sets the session object.
+  useEffect(() => {
+    if (!session) {
+      dispatch({
+        type: SET_SESSION,
+        data: prologSession,
+      })
+    }
+  }, [session, dispatch])
+
+  useEffect(() => {
+    const loadProgram = async () => {
+      await session.promiseConsult(KnowledgeBase)
+
+      setIsProgramLoaded(true)
+    }
+
+    if (session) {
+      loadProgram()
+    }
+  }, [session])
+
+  if (!session || !isProgramLoaded) {
+    return <h1>Loading...</h1>
+  }
+
+  return <>{children}</>
+}
+
+export default PrologSessionWrapper
