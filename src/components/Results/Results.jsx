5fc2c68fce832755e769a7e820e434df94438743
Leonel Aguirre <leonel.aguirre77@gmail.com>
2022-11-23 22:27:27 -0600

    :sparkles: Results page now shows winning house.

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,18 +1,43 @@
+import { useEffect, useState } from "react"
+
 import { useSession } from "../../context/SessionProvider"
+import useProlog from "../../hooks/useProlog"
 
 const Results = () => {
-  const { categoryConfigurationList, currentCategoryIndex, currentQuestionID } =
-    useSession()
+  const [winningHouse, setWinnindHouse] = useState("")
+
+  const { query } = useProlog()
+
+  const {
+    categoryConfigurationList,
+    currentCategoryIndex,
+    currentQuestionID,
+    currentScore,
+  } = useSession()
 
   console.log({
     categoryConfigurationList,
     currentCategoryIndex,
     currentQuestionID,
+    currentScore,
   })
+
+  useEffect(() => {
+    query(`determine_house(${currentScore}, H).`, (result) => {
+      const formattedResult = result
+        .replaceAll(",,", ",~")
+        .replaceAll(/\[|\]|,|H = /g, "")
+        .replaceAll("~", ",")
+
+      setWinnindHouse(formattedResult)
+    })
+  }, [currentScore])
 
   return (
     <div className="results">
       <h1>Results</h1>
+      <p>Score: {currentScore}</p>
+      <p>{winningHouse}</p>
     </div>
   )
 }
