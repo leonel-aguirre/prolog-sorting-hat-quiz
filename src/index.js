2671bf00df2ac260a46447bfd16dd28183237dae
Leonel Aguirre <leonel.aguirre77@gmail.com>
2022-11-26 01:30:58 -0600

    :construction: Firebase database read and write POC working.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,41 @@ import ReactDOM from "react-dom/client"
 
 import App from "./components/App"
 import reportWebVitals from "./reportWebVitals"
+import {
+  database,
+  set,
+  ref,
+  onValue,
+  getAuth,
+  signInAnonymously,
+} from "./firebase"
 
+const auth = getAuth()
+
+;(async () => {
+  try {
+    const userCredentials = await signInAnonymously(auth)
+
+    console.log({ userCredentials })
+  } catch (error) {
+    console.error(error)
+  }
+
+  console.log("Writing data")
+
+  set(ref(database, "/users/" + 1234), {
+    username: "Test",
+    email: "test@test.com",
+  })
+
+  const userRef = ref(database, "users/" + 1)
+
+  onValue(userRef, (snapshot) => {
+    const data = snapshot.val()
+
+    console.log({ data })
+  })
+})()
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
